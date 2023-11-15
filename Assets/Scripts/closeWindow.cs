4578390649e6d61c40a1920cac3de0f59e42d416
sou004002 <shuwashuwa_0402@i.softbank.jp>
2023-11-16 00:49:02 +0900

    v1.0.0

---
+++ Assets/Scripts/closeWindow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class closeWindow : MonoBehaviour
+{
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        
+    }
+}
